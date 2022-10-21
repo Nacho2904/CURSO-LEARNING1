504974d9ae8892f8c32ab6f102aa6605f8ba148c
Nacho2904 <96963299+Nacho2904@users.noreply.github.com>
2022-10-21 11:44:29 -0300

    Add files via upload

--- script_molinos.sh
+++ script_molinos.sh
@@ -1,2 +1,4 @@
-wget https://github.com/Nacho2904/CURSO-LEARNING1/blob/tareas/modelo_molinos.h5
-python3 $1
+wget https://github.com/Nacho2904/CURSO-LEARNING1/blob/tareas/modelo_molinos.h5?raw=true
+mv modelo_molinos.h5?raw=true modelo_molinos.h5
+wget https://raw.githubusercontent.com/Nacho2904/CURSO-LEARNING1/tareas/script_molinos.py
+python3 script_molinos.py $1
